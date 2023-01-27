99b10ca1751bfd4a72d42a6ba4ccfb561f276c7f
Pedro Palacios Almendros <palmenros@gmail.com>
2023-01-27 17:28:31 +0100

    Set up scaffolding and ANTLR 4 project

---
+++ compiler/src/main.cpp
@@ -0,0 +1,36 @@
+/* Copyright (c) 2012-2017 The ANTLR Project. All rights reserved.
+ * Use of this file is governed by the BSD 3-clause license that
+ * can be found in the LICENSE.txt file in the project root.
+ */
+
+//
+//  main.cpp
+//  antlr4-cpp-demo
+//
+//  Created by Mike Lischke on 13.03.16.
+//
+
+#include <iostream>
+
+#include "antlr4-runtime.h"
+#include "TLexer.h"
+#include "TParser.h"
+
+using namespace antlrcpptest;
+using namespace antlr4;
+
+int main(int argc, const char * argv[]) {
+
+//    ANTLRInputStream input("a = b + \"c\";(((x * d))) * e + f; a + (x * (y ? 0 : 1) + z);");
+    ANTLRInputStream input("a = pedro; c = h + d * b;");
+    TLexer lexer(&input);
+    CommonTokenStream tokens(&lexer);
+
+    TParser parser(&tokens);
+    tree::ParseTree *tree = parser.main();
+
+    auto s = tree->toStringTree(&parser);
+    std::cout << "Parse Tree: " << s << std::endl;
+
+    return 0;
+}
